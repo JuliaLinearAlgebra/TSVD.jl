125f3b218e57d3a0ac91fe3cb17b116da07d90ce
Andreas Noack <andreasnoackjensen@gmail.com>
2018-08-23 17:43:40 +0200

    Work around https://discourse.julialang.org/t/mkdocs-material-in-documenter/13764/2

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ using Documenter, TSVD
 makedocs()
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-material"),
+    deps   = Deps.pip("mkdocs==0.17.5", "python-markdown-math", "mkdocs-material==2.9.4"),
     repo = "github.com/andreasnoack/TSVD.jl.git",
     julia  = "1.0"
 )
